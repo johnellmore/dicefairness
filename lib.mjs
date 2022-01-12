7d0c308cd4fab15504559049e7a4ac50573e14d6
John Ellmore <jnellmore@gmail.com>
2022-01-11 22:37:39 -0600

    Building out the initial calculation functions

---
+++ lib.mjs
@@ -0,0 +1,88 @@
+export class RollSet {
+  constructor(rollCounts) {
+    this.rollCounts = rollCounts;
+  }
+
+  get numRolls() {
+    let sum = 0;
+    this.rollCounts.forEach(numForRoll => sum += numForRoll);
+    return sum;
+  }
+
+  get rollTypes() {
+    return new Set(this.rollCounts.keys());
+  }
+}
+
+export function parseDiceRolls(inputStr) {
+  const lines = inputStr.split("\n");
+  const sanitizedLines = lines
+    .map(l => l.replace(/^\W+/g, '').replace(/\W+$/g, ''));
+  const validLines = sanitizedLines.filter(l => !!l);
+  return makeRollSetFromRolls(validLines);
+}
+
+export function makeRollSetFromRolls(rollsArr) {
+  const counts = rollsArr
+    .reduce((m, roll) =>
+      m.set(roll, m.has(roll) ? m.get(roll) + 1 : 1),
+      new Map()
+    );
+  return new RollSet(counts);
+}
+
+export function guessDiceType(rollSet) {
+  const areRollsNumeric = [...rollSet.rollTypes].every(v => v.match(/^\d+$/));
+  if (areRollsNumeric) {
+    const numericRolls = [...rollSet.rollTypes].map(r => +r).sort((a, b) => a - b);
+    const min = numericRolls[0];
+    const max = numericRolls[numericRolls.length - 1];
+    const range = max - min + 1;
+
+    if (range <= 2 && max <= 2 && rollSet.numRolls >= 4) {
+      return {type: 'd2', sides: 2 };
+    }
+
+    if (range <= 4 && max <= 4 && rollSet.numRolls >= 6) {
+      return {type: 'd4', sides: 4 };
+    }
+
+    if (range <= 6 && max <= 6 && rollSet.numRolls >= 9) {
+      return {type: 'd6', sides: 6 };
+    }
+
+    if (range <= 8 && max <= 8 && rollSet.numRolls >= 12) {
+      return {type: 'd8', sides: 8 };
+    }
+
+    if (range <= 10 && max <= 10 && rollSet.numRolls >= 15) {
+      return {type: 'd10', sides: 10 };
+    }
+
+    if (range <= 12 && max <= 12 && rollSet.numRolls >= 15) {
+      return {type: 'd12', sides: 12 };
+    }
+
+    if (range <= 20 && max <= 20 && rollSet.numRolls >= 15) {
+      return {type: 'd20', sides: 20 };
+    }
+  }
+
+  const rollsToSides = rollSet.numRolls / rollSet.rollTypes.size;
+  if (rollsToSides >= 4) {
+    return {type: 'custom', sides: rollSet.rollTypes.size };
+  }
+
+  return null;
+}
+
+export function chiSquared(rollSet, sides) {
+  // get an array of the different roll totals, where every side that WASN'T
+  // rolled is given a 0
+  const totals = [...rollSet.rollCounts.values(), ...Array(sides).fill(0)]
+    .slice(0, sides);
+  const nexp = rollSet.numRolls / sides;
+  const chiSq = totals
+    .reduce((chi, nk) => chi + (Math.pow(nk - nexp, 2) / nexp), 0);
+  return chiSq;
+}
---
+++ tests.mjs
@@ -0,0 +1,154 @@
+import { chiSquared, guessDiceType, makeRollSetFromRolls, parseDiceRolls, RollSet } from './lib.mjs';
+
+test('parseDiceRolls() parses a set of clean d6 inputs', () => {
+  const input = "5\n1\n4\n1\n4\n2\n1\n3\n5\n4\n";
+  const expectedCounts = new Map([
+    ['5', 2],
+    ['1', 3],
+    ['4', 3],
+    ['2', 1],
+    ['3', 1]
+  ]);
+  const actual = parseDiceRolls(input);
+  assert(actual.rollCounts.size === expectedCounts.size);
+  actual.rollCounts.forEach(
+    (numRolls, roll) => assert(expectedCounts.get(roll) === numRolls)
+  );
+});
+
+test('parseDiceRolls() parses a messy set of coin flips', () => {
+  const input = "heads\nheads\nheads \n tails \ntails\n heads\ntails\n\nheads\nheads\ntails\n\n";
+  const expectedCounts = new Map([
+    ['heads', 6],
+    ['tails', 4],
+  ]);
+  const actual = parseDiceRolls(input);
+  assert(actual.rollCounts.size === expectedCounts.size);
+  actual.rollCounts.forEach(
+    (numRolls, roll) => assert(expectedCounts.get(roll) === numRolls)
+  );
+});
+
+test('RollSet reports num rolls and types of rolls correctly', () => {
+  const rollCounts = new Map([
+    ['5', 2],
+    ['1', 3],
+    ['4', 3],
+    ['2', 1],
+    ['3', 1]
+  ]);
+  const rollSet = new RollSet(rollCounts);
+
+  assert(rollSet.numRolls === 10);
+
+  const expectedRollTypes = new Set(['1', '2', '3', '4', '5']);
+  assert(expectedRollTypes.size === rollSet.rollTypes.size);
+  for (var expectedType of expectedRollTypes) {
+    assert(rollSet.rollTypes.has(expectedType));
+  }
+});
+
+test('guessDiceType() is indeterminate when appropriate', () => {
+  const noRollsGuess = guessDiceType(makeRollSetFromRolls([]));
+  assert(noRollsGuess === null);
+
+  const justOneRollGuess = guessDiceType(makeRollSetFromRolls(['1']));
+  assert(justOneRollGuess === null);
+
+  const smallSetGuess = guessDiceType(makeRollSetFromRolls(['1', '5', '6']));
+  assert(smallSetGuess === null);
+});
+
+test('guessDiceType() identifies coin flips', () => {
+  const rolls1 = shuffle([...Array(6).fill('heads'), ...Array(4).fill('tails')]);
+  const guess1 = guessDiceType(makeRollSetFromRolls(rolls1));
+  assert(guess1 !== null);
+  assert(guess1.type === 'custom');
+  assert(guess1.sides === 2);
+
+  const rolls2 = shuffle([...Array(7).fill('h'), ...Array(3).fill('t')]);
+  const guess2 = guessDiceType(makeRollSetFromRolls(rolls2));
+  assert(guess2 !== null);
+  assert(guess2.type === 'custom');
+  assert(guess2.sides === 2);
+});
+
+test('guessDiceType() identifies standard dice types', () => {
+  const d4 = ['2', '2', '4', '3', '4', '2', '1', '2'];
+  const d4Guess = guessDiceType(makeRollSetFromRolls(d4));
+  assert(d4Guess !== null);
+  assert(d4Guess.type === 'd4');
+  assert(d4Guess.sides === 4);
+
+  const d6 = ['4', '2', '3', '4', '3', '6', '6', '3', '1', '5'];
+  const d6Guess = guessDiceType(makeRollSetFromRolls(d6));
+  assert(d6Guess !== null);
+  assert(d6Guess.type === 'd6');
+  assert(d6Guess.sides === 6);
+
+  // These random rolls in partiular are interesting because they don't have any
+  // lower values. The guesser should still notice that they are numeric, and
+  // that they range up to 8 though.
+  const d8 = ['6', '8', '6', '3', '8', '8', '6', '7', '6', '8', '4', '6'];
+  const d8Guess = guessDiceType(makeRollSetFromRolls(d8));
+  assert(d8Guess !== null);
+  assert(d8Guess.type === 'd8');
+  assert(d8Guess.sides === 8);
+
+  const d10 = ['5', '4', '5', '9', '8', '8', '0', '9', '5', '7', '8', '6', '3', '8', '9'];
+  const d10Guess = guessDiceType(makeRollSetFromRolls(d10));
+  assert(d10Guess !== null);
+  assert(d10Guess.type === 'd10');
+  assert(d10Guess.sides === 10);
+
+  const d12 = ['10', '10', '4', '4', '10', '12', '1', '10', '1', '9', '10', '11', '5', '11', '11', '9', '12', '12'];
+  const d12Guess = guessDiceType(makeRollSetFromRolls(d12));
+  assert(d12Guess !== null);
+  assert(d12Guess.type === 'd12');
+  assert(d12Guess.sides === 12);
+
+  const d20 = ['9', '14', '12', '8', '4', '11', '7', '6', '15', '15', '14', '14', '17', '19', '1', '4', '19', '5', '6', '18', '11', '6', '17', '5', '9', '12', '20', '1', '6', '2'];
+  const d20Guess = guessDiceType(makeRollSetFromRolls(d20));
+  assert(d20Guess !== null);
+  assert(d20Guess.type === 'd20');
+  assert(d20Guess.sides === 20);
+});
+
+test('guessDiceType() identifies any collection of side types', () => {
+  const rolls = ['b', 'b', 'd', 'c', 'd', 'b', 'a', 'b', 'b', 'b', 'd', 'c', 'd', 'b', 'a', 'b'];
+  const guess = guessDiceType(makeRollSetFromRolls(rolls));
+  assert(guess !== null);
+  assert(guess.type === 'custom');
+  assert(guess.sides === 4);
+});
+
+test('chiSquared() reports zero for a uniformly-distributed set of dice rolls', () => {
+  // build a set of 100 d20 rolls, where each side was rolled exactly 5 times
+  const rolls = new RollSet(new Map(Array(20).fill(0).map((_, i) => ['' + (i + 1), 5])));
+  assert(chiSquared(rolls, 20) === 0);
+});
+
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+function assert(cond) {
+  if (!cond) {
+    throw new Error('Assertion failed');
+  }
+}
+
+function test(desc, func) {
+  try {
+    func();
+  } catch (e) {
+    console.error(`❌ ${desc}`);
+    console.error(e);
+    return;
+  }
+  console.log(`✔️ ${desc}`);
+}
